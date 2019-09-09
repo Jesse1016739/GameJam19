e3dfd8aaa1be4850ff3f011541890cb8d0fe40e9
Kyan <rowse9@gmail.com>
2019-09-09 23:55:26 +1000

    Updated FMOD Banks

---
+++ FMODTEST/Assets/fmod.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fmod : MonoBehaviour
+{
+    FMOD.Studio.EventInstance AMB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AMB = FMODUnity.RuntimeManager.CreateInstance(""); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
